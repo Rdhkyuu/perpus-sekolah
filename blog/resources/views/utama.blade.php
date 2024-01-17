4bcbc7b8972ba9a1a0075fab5de57f46e5f82423
Rikyuu <106382785+Rdhkyuu@users.noreply.github.com>
2024-01-17 14:47:05 +0800

    Nambah beberapa kata di file utama

--- blog/resources/views/utama.blade.php
+++ blog/resources/views/utama.blade.php
@@ -8,12 +8,8 @@
 <body>
     <h1>
         Hai!!! Haloo!!!
-
-<<<<<<< HEAD
-        Ini halaman utamaa yaa!!
-=======
+        Muahahaha!
         Tambahan dari pak khusairi
->>>>>>> f0c153cfb5bd317ee72f7b96e149da06262e12f0
     </h1>
     
 </body>
