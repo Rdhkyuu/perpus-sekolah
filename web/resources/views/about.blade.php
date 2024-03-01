0065c17077d91c3da5bd24779a4d9b9fee825077
Rikyuu <thecreepercraftindo31@gmail.com>
2024-03-01 08:01:49 +0800

    Mengisi halaman about

--- web/resources/views/about.blade.php
+++ web/resources/views/about.blade.php
@@ -3,9 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styleabout.css') }}">
     <title>Halaman About</title>
 </head>
 <body>
-    <h1>Perpustakaan Sekolah</h1>
+<div class="responsive-container-block bigContainer">
+  <div class="responsive-container-block Container">
+    <a id= "back" href="/" target="" rel="noopener noreferrer"> < Kembali ke halaman awal</a>
+    <p class="text-blk heading">
+      About Us
+    </p>
+    <p class="text-blk subHeading">
+      Dibuat untuk tugas akhir sekolah. Perpustakaan sekolah itu sendiri adalah seperti namanya, dimana perpustakaan ini ruang lingkupnya di sekolah. Untuk membantu siswa-siwi dan petugas sekolah agar memudahkan meminjam buku diperpustakaan. Cek sosial media kami masing-masing di bawah ini.
+    </p>
+    <div class="social-icons-container">
+      <a class="social-icon" href="https://www.instagram.com/rdhkyu/" target="_blank">
+        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png">
+      </a>
+      <a class="social-icon" href="https://www.instagram.com/ar_fan.h/" target="_blank">
+        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png">
+      </a>
+      <a class="social-icon" href="https://www.instagram.com/dywendy__/" target="_blank">
+        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png">
+      </a>
+      <a class="social-icon" href="https://www.instagram.com/cindyarr__/" target="_blank">
+        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png">
+      </a>
+      <a class="social-icon" href="https://www.instagram.com/auliaa.qa/" target="_blank">
+        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png">
+      </a>
+      <a class="social-icon">
+        <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png">
+      </a>
+    </div>
+  </div>
+</div>
 </body>
 </html>
